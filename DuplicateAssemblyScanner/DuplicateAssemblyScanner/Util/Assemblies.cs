fa14b97406d7ddcae87c31dfc94d56e70771a023
Aubergine <guy.fraser1@gmail.com>
2020-03-04 02:51:05 +0000

    Now logs which mods contain the duplicate assemblies

--- DuplicateAssemblyScanner/DuplicateAssemblyScanner/Util/Assemblies.cs
+++ DuplicateAssemblyScanner/DuplicateAssemblyScanner/Util/Assemblies.cs
@@ -1,9 +1,15 @@
 namespace DuplicateAssemblyScanner.Util {
+    using ColossalFramework;
+    using ColossalFramework.Plugins;
+    using ICities;
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Reflection;
     using System.Text;
+    using UnityEngine.SceneManagement;
+    using static ColossalFramework.Plugins.PluginManager;
 
     public class Assemblies {
 
@@ -14,15 +20,13 @@ namespace DuplicateAssemblyScanner.Util {
         /// 
         /// <returns>Dictionary of assembly lists keyed by assembly name.</returns>
         public static Dictionary<string, List<string>> Scan(out int problemsFound) {
-
+            Log.Info("Scanning app domain assemblies for duplicates...");
             problemsFound = 0;
 
             // assembly name -> list of assemblies with that name
             Dictionary<string, List<string>> results = new Dictionary<string, List<string>>();
 
-            StringBuilder log = new StringBuilder(1500);
-
-            log.Append("Scanning assemblies...");
+            bool detailedLogging = SceneManager.GetActiveScene().name != "Game";
 
             Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
@@ -38,25 +42,70 @@ namespace DuplicateAssemblyScanner.Util {
 
                         if (matches.Count == 1) {
                             ++problemsFound;
-                            log.Append($"\n  Duplicate: '{name}' v{matches.First<string>()}");
+                            if (detailedLogging) {
+                                FindModsWithAssembly(name);
+                            }
                         }
-
                         matches.Add(ver);
-                        log.Append($"\n  Duplicate: '{name}' v{ver}");
 
                     } else {
+
                         results.Add(name, new List<string>() {
                             { ver }
                         });
+
+                    }
+                }
+                catch (Exception e) {
+                    Log.Error(e.ToString());
+                }
+            }
+
+            Log.Info($"{problemsFound} problem(s) found.");
+            return results;
+        }
+
+        internal static void FindModsWithAssembly(string matchName) {
+            Log.Info($"# Duplicate '{matchName}' assembly exists in mods:");
+
+            PluginManager manager = Singleton<PluginManager>.instance;
+
+            List<PluginInfo> mods = new List<PluginInfo>(manager.modCount);
+
+            mods.AddRange(manager.GetPluginsInfo()); // normal mods
+            mods.AddRange(manager.GetCameraPluginInfos()); // camera scripts
+
+            List<Assembly> contents;
+            string name;
+            string ver;
+            string path;
+
+            // iterate plugins
+            foreach (PluginInfo mod in mods) {
+                try {
+                    contents = mod.GetAssemblies();
+                    foreach (Assembly asm in contents) {
+                        name = asm.GetName().Name;
+                        if (name == matchName) {
+                            ver = asm.GetName().Version.ToString();
+                            path = Path.GetFileName(mod.modPath);
+                            name = GetNameOfMod(mod);
+
+                            Log.Info($"  - v{ver} /{path} '{name}'");
+                        }
                     }
                 } catch (Exception e) {
                     Log.Error(e.ToString());
                 }
             }
+        }
 
-            Log.Info(log.ToString());
-
-            return results;
+        internal static string GetNameOfMod(PluginInfo mod) {
+            try {
+                return mod?.userModInstance != null ? ((IUserMod)mod.userModInstance).Name : string.Empty;
+            } catch {
+                return "Error getting mod name!";
+            }
         }
     }
 }
